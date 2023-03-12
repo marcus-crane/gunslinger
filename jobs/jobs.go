5aec40dd27f6ae826c7427ff3477fbde3dee3e27
Marcus Crane <marcus@utf9k.net>
2023-03-12 23:42:25 +1300

    Reduce polling for manga
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- jobs/jobs.go
+++ jobs/jobs.go
@@ -22,7 +22,7 @@ func SetupInBackground(database *sqlx.DB) *gocron.Scheduler {
 	s := gocron.NewScheduler(time.UTC)
 
 	s.Every(1).Seconds().Do(GetCurrentlyPlayingPlex, database)
-	s.Every(5).Seconds().Do(GetRecentlyReadManga, database)
+	s.Every(1).Minutes().Do(GetRecentlyReadManga, database)
 	s.Every(30).Seconds().Do(GetCurrentlyPlayingSteam, database)
 
 	// Assuming we have just redeployed or have crashed, we will
