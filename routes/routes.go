f4fd49cfe093c4c3072f80dcf5cba5cbd5ce51ac
Marcus Crane <marcus@utf9k.net>
2023-04-02 21:07:11 +1200

    Establish a very basic v4 set of endpoints
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- routes/routes.go
+++ routes/routes.go
@@ -57,6 +57,10 @@ func Register(mux *http.ServeMux, database *sqlx.DB) http.Handler {
 		renderJSONMessage(w, "This is the v3 endpoint of the API")
 	})
 
+	mux.HandleFunc("/api/v4", func(w http.ResponseWriter, r *http.Request) {
+		renderJSONMessage(w, "This is the v4 endpoint of the API")
+	})
+
 	mux.HandleFunc("/api/v3/playing", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(jobs.CurrentPlaybackItem)
@@ -68,6 +72,58 @@ func Register(mux *http.ServeMux, database *sqlx.DB) http.Handler {
 	})
 
 	mux.HandleFunc("/api/v3/history", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		var response []models.ResponseMediaItem
+		var result []models.DBMediaItem
+		// If nothing is playing, the "now playing" will likely be the same as the
+		// first history item so we skip it if now playing and index 0 of history match.
+		// We don't fully do an offset jump though as an item is only committed to the DB
+		// when it changes to inactive so we don't want to hide a valid item in that state
+		if err := database.Select(&result, "SELECT * FROM db_media_items ORDER BY created_at desc LIMIT 7"); err != nil {
+			json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
+			return
+		}
+		for idx, item := range result {
+			// A valid case is when I just listen to the same song over and over so
+			// we need to ensure we're in the right state to skip historical items
+			if idx == 0 && item.Title == jobs.CurrentPlaybackItem.Title && jobs.CurrentPlaybackItem.Backfilled {
+				continue
+			}
+			rItem := models.ResponseMediaItem{
+				OccuredAt: time.Unix(item.CreatedAt, 0).Format(time.RFC3339),
+				Title:     item.Title,
+				Subtitle:  item.Subtitle,
+				Category:  item.Category,
+				Source:    item.Source,
+				Image:     item.Image,
+			}
+			response = append(response, rItem)
+		}
+		json.NewEncoder(w).Encode(response)
+	})
+
+	mux.HandleFunc("/api/v4/playing", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		var result models.DBMediaItem
+		if err := database.Get(&result, "SELECT * FROM db_media_items ORDER BY created_at desc LIMIT 1"); err != nil {
+			json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
+			return
+		}
+
+		playbackItems := []models.ResponseMediaItem{
+			{
+				OccuredAt: time.Unix(result.CreatedAt, 0).Format(time.RFC3339),
+				Title:     result.Title,
+				Subtitle:  result.Subtitle,
+				Category:  result.Category,
+				Source:    result.Source,
+				Image:     result.Image,
+			},
+		}
+		json.NewEncoder(w).Encode(playbackItems)
+	})
+
+	mux.HandleFunc("/api/v4/history", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		var response []models.ResponseMediaItem
 		var result []models.DBMediaItem
